e6dadf7814d7e0bc774f09525cec3d37aa51e5d9
juankost <juan.kostelec@gmail.com>
2024-03-15 17:52:42 +0100

    Fixed the authentication process

---
+++ app/auth/AuthProvider.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import * as React from 'react'
+import {
+  IdTokenResult,
+  onIdTokenChanged,
+  User as FirebaseUser
+} from 'firebase/auth'
+import { filterStandardClaims } from 'next-firebase-auth-edge/lib/auth/claims'
+import { AuthContext, User } from './AuthContext'
+import { getFirebaseAuth } from './firebase'
+import { login, logout } from '@/app/auth'
+
+export interface AuthProviderProps {
+  serverUser: User | null
+  children: React.ReactNode
+}
+
+function toUser(user: FirebaseUser, idTokenResult: IdTokenResult): User {
+  return {
+    ...user,
+    emailVerified:
+      user.emailVerified || (idTokenResult.claims.email_verified as boolean),
+    customClaims: filterStandardClaims(idTokenResult.claims),
+    authTime: toAuthTime(idTokenResult.issuedAtTime)
+  }
+}
+
+function toAuthTime(date: string) {
+  return new Date(date).getTime() / 1000
+}
+
+export const AuthProvider: React.FunctionComponent<AuthProviderProps> = ({
+  serverUser,
+  children
+}) => {
+  const [user, setUser] = React.useState(serverUser)
+
+  React.useEffect(() => {
+    if (user === serverUser) {
+      return
+    }
+
+    setUser(serverUser)
+  }, [serverUser])
+
+  const handleLogout = async () => {
+    if (!user) {
+      return
+    }
+
+    await logout()
+    window.location.reload()
+  }
+
+  const handleLogin = async (firebaseUser: FirebaseUser) => {
+    const idTokenResult = await firebaseUser.getIdTokenResult()
+
+    if (
+      user?.authTime &&
+      user.authTime >= toAuthTime(idTokenResult.issuedAtTime)
+    ) {
+      return
+    }
+
+    await login(idTokenResult.token)
+    setUser(toUser(firebaseUser, idTokenResult))
+  }
+
+  const handleIdTokenChanged = async (firebaseUser: FirebaseUser | null) => {
+    if (!firebaseUser) {
+      await handleLogout()
+      return
+    }
+
+    await handleLogin(firebaseUser)
+  }
+
+  React.useEffect(() => {
+    return onIdTokenChanged(getFirebaseAuth(), handleIdTokenChanged)
+  }, [user])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -15,6 +15,19 @@ module.exports = {
         pathname: '**',
       },
     ]
-  }
+  },
+  future: {
+    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
+      // Looks like backward compatibility approach.
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
 }
 
